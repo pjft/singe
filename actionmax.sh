da491925452a7cd92a728e8e2d77276dbc1ca3fc
DirtBagXon <noop@member.fsf.org>
2020-01-25 11:36:04 +0000

    Output startup script mgs to STDERR, to help launcher app

--- actionmax.sh
+++ actionmax.sh
@@ -5,6 +5,10 @@ if realpath / >/dev/null; then SCRIPT_DIR=$(realpath "$SCRIPT_DIR"); fi
 SINGE_BIN=singe.bin
 DAPHNE_SHARE=~/.daphne
 
+function STDERR () {
+	/bin/cat - 1>&2
+}
+
 echo "ActionMax Launcher : Script dir is $SCRIPT_DIR"
 cd "$SCRIPT_DIR"
 
@@ -17,9 +21,9 @@ if [ "$1" = "-fullscreen" ]; then
 fi
 
 if [ -z "$1" ] ; then
-    echo "Specify a game to try: "
+    echo "Specify a game to try: " | STDERR
     echo
-    echo  -e "\t$0 [-fullscreen] <gamename>"
+    echo "$0 [-fullscreen] <gamename>" | STDERR
 
     for game in 38ambushalley bluethunder hydrosub2021 popsghostly sonicfury; do
 	if ls ~/.daphne/singe/actionmax/$game >/dev/null 2>&1; then
@@ -30,8 +34,8 @@ if [ -z "$1" ] ; then
     done
     if [ "$uninstalled" ]; then
 	echo
-	echo "Games not found in ~/.daphne/singe/actionmax: "
-	echo "$uninstalled" | fold -s -w60 | sed 's/^ //; s/^/\t/'
+	echo "Games not found in ~/.daphne/singe/actionmax: " | STDERR
+	echo "$uninstalled" | fold -s -w60 | sed 's/^ //; s/^/\t/' | STDERR
     fi
     if [ -z "$installed" ]; then
 	cat <<EOF 
@@ -40,16 +44,16 @@ Error: Please put the required files in ~/.daphne/singe/actionmax
 EOF
     else   
 	echo
-	echo "Games available: "
-	echo "$installed" | fold -s -w80 | sed 's/^ //; s/^/\t/'
+	echo "Games available: " | STDERR
+	echo "$installed" | fold -s -w80 | sed 's/^ //; s/^/\t/' | STDERR
     fi
     exit 1
 fi
 
 if [ ! -f $DAPHNE_SHARE/singe/actionmax/$1/$1.singe ] || [ ! -f $DAPHNE_SHARE/singe/actionmax/$1/frame_$1.txt ]; then
 	echo 
-	echo "Missing file: $DAPHNE_SHARE/singe/actionmax/$1/$1.singe ?"
-	echo "              $DAPHNE_SHARE/singe/actionmax/$1/frame_$1.txt ?"
+	echo "Missing file: $DAPHNE_SHARE/singe/actionmax/$1/$1.singe ?" | STDERR
+	echo "              $DAPHNE_SHARE/singe/actionmax/$1/frame_$1.txt ?" | STDERR
 	echo 
 	exit 1
 fi
@@ -71,11 +75,12 @@ EXIT_CODE=$?
 if [ "$EXIT_CODE" -ne "0" ] ; then
 	if [ "$EXIT_CODE" -eq "127" ]; then
 		echo ""
-		echo "SINGE failed to start."
-		echo "This is probably due to a library problem."
-		echo "Run ./singe.bin directly to see which libraries are missing."
+		echo "SINGE failed to start." | STDERR
+		echo "This is probably due to a library problem." | STDERR
+		echo "Run ./singe.bin directly to see which libraries are missing." | STDERR
 		echo ""
 	else
-		echo "Loader failed with an unknown exit code : $EXIT_CODE."
+		echo "Loader failed with an unknown exit code : $EXIT_CODE." | STDERR
 	fi
+	exit $EXIT_CODE
 fi
--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 SCRIPT_DIR=`dirname "$0"`
 if realpath / >/dev/null; then SCRIPT_DIR=$(realpath "$SCRIPT_DIR"); fi
 SINGE_BIN=singe.bin
 DAPHNE_SHARE=~/.daphne
+
+function STDERR () {
+	/bin/cat - 1>&2
+}
 
 echo "Singe Launcher : Script dir is $SCRIPT_DIR"
 cd "$SCRIPT_DIR"
@@ -17,9 +21,9 @@ if [ "$1" = "-fullscreen" ]; then
 fi
 
 if [ -z $1 ] ; then
-	echo "Specify a game to try: "
+	echo "Specify a game to try: " | STDERR
 	echo
-	echo "\t$0 [-fullscreen] <gamename>"
+	echo "$0 [-fullscreen] <gamename>" | STDERR
 	echo
 
         echo "Games available: "
@@ -28,15 +32,15 @@ if [ -z $1 ] ; then
 			installed="$installed $game"
 		fi
         done
-        echo "$installed" | fold -s -w60 | sed 's/^ //; s/^/\t/'
+        echo "$installed" | fold -s -w60 | sed 's/^ //; s/^/\t/' | STDERR
 	echo
-	exit
+	exit 1
 fi
 
 if [ ! -f $DAPHNE_SHARE/singe/$1/$1.singe ] || [ ! -f $DAPHNE_SHARE/singe/$1/$1.txt ]; then
         echo 
-        echo "Missing file: $DAPHNE_SHARE/singe/$1/$1.singe ?"
-        echo "              $DAPHNE_SHARE/singe/$1/$1.txt ?"
+        echo "Missing file: $DAPHNE_SHARE/singe/$1/$1.singe ?" | STDERR
+        echo "              $DAPHNE_SHARE/singe/$1/$1.txt ?" | STDERR
         echo 
         exit 1
 fi
@@ -60,11 +64,12 @@ EXIT_CODE=$?
 if [ "$EXIT_CODE" -ne "0" ] ; then
 	if [ "$EXIT_CODE" -eq "127" ]; then
 		echo ""
-		echo "SINGE failed to start."
-		echo "This is probably due to a library problem."
-		echo "Run ./singe.bin directly to see which libraries are missing."
+		echo "SINGE failed to start." | STDERR
+		echo "This is probably due to a library problem." | STDERR
+		echo "Run ./singe.bin directly to see which libraries are missing." | STDERR
 		echo ""
 	else
-		echo "Loader failed with an unknown exit code : $EXIT_CODE."
+		echo "Loader failed with an unknown exit code : $EXIT_CODE." | STDERR
 	fi
+	exit $EXIT_CODE
 fi
